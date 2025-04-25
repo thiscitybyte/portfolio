27aec277c6dd6c1db12edb0b6313060adaf3e76c
Eleni Koundouraki <70891079+elenikoundouraki@users.noreply.github.com>
2025-04-25 15:11:26 +0300

    Updated content config

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,17 @@
 import { z, defineCollection } from 'astro:content'
 import { glob } from 'astro/loaders'
 
-const blog = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/blog' }),
-	schema: () =>
-		z.object({
-			title: z.string(),
-			description: z.string(),
-			pubDate: z.date(),
-			image: z.string(),
-			author: z.string(),
-			badges: z.array(z.string())
-		})
-})
-
-const eguides = defineCollection({
-	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/eguides' }),
+const caseStudies = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/caseStudies' }),
 	schema: () =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
 			image: z.string(),
-			readingTime: z.string(),
-			badges: z.array(z.string())
+			roles: z.array(z.string())
 		})
 })
 
 export const collections = {
-	blog,
-	eguides
+	caseStudies
 }
