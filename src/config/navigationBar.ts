5364dec7edb5a34d8e8a1bc913559802f2573e4c
Eleni Koundouraki <70891079+elenikoundouraki@users.noreply.github.com>
2025-05-01 12:54:13 +0300

    Updated ts files

--- src/config/navigationBar.ts
+++ src/config/navigationBar.ts
@@ -2,45 +2,19 @@
 // ------------
 // Description: The navigation bar data for the website.
 
-export interface Logo {
-	src: string
-	alt: string
-	text: string
-}
-
 export interface NavSubLink {
 	name: string
 	link: string
-	badge?: string
-}
-
-export interface NavSubItem {
-	name?: string
-	icon?: string
-	links: NavSubLink[]
 }
 
 export interface NavItem {
 	name: string
 	link: string
-	submenu?: NavSubItem[]
 	align?: string
-	badge?: string
-}
-
-export interface NavAction {
-	name: string
-	link?: string
-	style: string
-	size: string
-	variation?: 'outline' | 'link'
-	modal?: string
 }
 
 export interface NavData {
-	logo: Logo
 	navItems: NavItem[]
-	navActions: NavAction[]
 }
 
 export async function getNavigationBarData() {
