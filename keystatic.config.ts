d3c6bd3f4954b9d1bb48f8934515a46c8367d596
Eleni Koundouraki <70891079+elenikoundouraki@users.noreply.github.com>
2025-05-01 12:21:30 +0300

    Updated Keystatic

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -3,10 +3,63 @@ import { config, fields, collection } from "@keystatic/core";
 export default config({
   storage: import.meta.env.DEV === true ? { kind: "local" } : { kind: "cloud" },
   cloud: {
-    project: "astro-starter-pro-theme/astro-starter-pro-astro-theme",
+    project: "milo-astro-theme/milo-astro-theme",
   },
   ui: {
-    brand: { name: "Oxygenna Themes" },
+    brand: { name: "Milo Astro Theme" },
   },
-  collections: {},
+  collections: {
+    caseStudies: collection({
+      label: "Case Studies",
+      slugField: "title",
+      path: "src/content/caseStudies/**",
+      format: { contentField: "body" },
+      schema: {
+        title: fields.slug({
+          name: {
+            label: "Title",
+            description: "The title of the case study",
+            validation: { isRequired: true },
+          },
+          slug: {
+            label: "SEO-friendly slug",
+            description:
+              "This will define the file/folder slug for this case study",
+          },
+        }),
+        description: fields.text({
+          label: "Description",
+          validation: { isRequired: true },
+        }),
+        cardImage: fields.image({
+          label: "Image URL",
+          directory: "public/caseStudies",
+          publicPath: "/caseStudies/",
+        }),
+        heroImage: fields.image({
+          label: "Hero Image URL",
+          directory: "public/caseStudies",
+          publicPath: "/caseStudies/",
+        }),
+        roles: fields.array(
+          fields.text({ label: "Role" }),
+
+          {
+            label: "Roles",
+            itemLabel: (props) => props.value,
+          }
+        ),
+        body: fields.markdoc({
+          label: "Body",
+          extension: "md",
+          options: {
+            image: {
+              directory: "public/caseStudies/",
+              publicPath: "/caseStudies/",
+            },
+          },
+        }),
+      },
+    }),
+  },
 });
