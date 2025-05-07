e8ed1c547d1fd32e197046b35a127d052cf4e5dc
Eleni Koundouraki <70891079+elenikoundouraki@users.noreply.github.com>
2025-05-07 13:50:41 +0300

    Small fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import lottie from "astro-integration-lottie";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
