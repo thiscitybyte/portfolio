a311402183ec23c3069036e975e02c797d64eab6
Eleni Koundouraki <70891079+elenikoundouraki@users.noreply.github.com>
2025-06-02 09:51:33 +0300

    Updated Keystatic config

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -23,40 +23,100 @@ export default config({
           },
           slug: {
             label: "SEO-friendly slug",
-            description: "This will define the file/folder slug for this case study",
+            description:
+              "This will define the file/folder slug for this case study",
           },
         }),
         description: fields.text({
           label: "Description",
+          multiline: true,
           validation: { isRequired: true },
         }),
         cardImage: fields.image({
-          label: "Image URL",
+          label: "Card Image",
           directory: "public/caseStudies",
-          publicPath: "/caseStudies/",
+          publicPath: "/caseStudies/design/",
+          validation: { isRequired: true },
+        }),
+        greyImage: fields.image({
+          label: "Grey Image",
+          directory: "public/caseStudies",
+          publicPath: "/caseStudies/design/",
+          validation: { isRequired: true },
         }),
         heroImage: fields.image({
-          label: "Hero Image URL",
+          label: "Hero Image",
           directory: "public/caseStudies",
-          publicPath: "/caseStudies/",
+          publicPath: "/caseStudies/design/",
+          validation: { isRequired: true },
         }),
-        roles: fields.array(
-          fields.text({ label: "Role" }),
-
+        roles: fields.array(fields.text({ label: "Role" }), {
+          label: "Roles",
+          itemLabel: (props) => props.value,
+        }),
+        introduction: fields.text({
+          label: "Introduction",
+          multiline: true,
+          validation: { isRequired: true },
+        }),
+        aboutProject: fields.object(
           {
-            label: "Roles",
-            itemLabel: (props) => props.value,
+            leftCol: fields.text({
+              label: "Left Column (HTML allowed)",
+              multiline: true,
+              validation: { isRequired: true },
+            }),
+            rightCol: fields.text({
+              label: "Right Column (HTML allowed)",
+              multiline: true,
+              validation: { isRequired: true },
+            }),
+          },
+          {
+            label: "About the Project",
+          }
+        ),
+        whatWeDid: fields.object(
+          {
+            leftCol: fields.text({
+              label: "Left Column (HTML allowed)",
+              multiline: true,
+              validation: { isRequired: true },
+            }),
+            rightCol: fields.text({
+              label: "Right Column (HTML allowed)",
+              multiline: true,
+              validation: { isRequired: true },
+            }),
+          },
+          {
+            label: "What We Did",
+          }
+        ),
+        images: fields.array(
+          fields.image({
+            label: "Image",
+            directory: "public/caseStudies",
+            publicPath: "/caseStudies/design/",
+          }),
+          {
+            label: "Images",
+          }
+        ),
+        clientFeedback: fields.array(
+          fields.object({
+            blockquote: fields.text({ label: "Blockquote" }),
+            figcaption: fields.text({ label: "Figcaption" }),
+            cite: fields.text({ label: "Cite" }),
+          }),
+          {
+            label: "Client Feedback",
+            itemLabel: () => "Client Feedback",
           }
         ),
         body: fields.markdoc({
-          label: "Body",
+          label: "Body (Not required)",
           extension: "md",
-          options: {
-            image: {
-              directory: "public/caseStudies/",
-              publicPath: "/caseStudies/",
-            },
-          },
         }),
       },
     }),
